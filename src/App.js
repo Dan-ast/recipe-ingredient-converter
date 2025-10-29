07d73110b4be4f7248572fe04d21948a5acc66c9
Anastasiia Dekret <anastasiiadekret@yahoo.com>
2025-10-29 16:23:15 +0100

    added basic recipe converter form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="conteiner">
+        <h1>Recipe Ingredient Converter</h1>
+        <form>
+          <div>
+            <label htmlFor="ingredient">Ingredient: </label>
+            <input type="text" id="ingredient" required/>
+          </div>
+
+          <div>
+            <label htmlFor="quantity">Quantity: </label>
+            <input type="number" id="quantity" step="any" required/>
+          </div>
+
+          <div>
+            <label htmlFor="unit">Unit: </label>
+            <select id="unit">
+              <option>Select a unit</option>
+              <option value="cup">Cup</option>
+              <option value="gram">Gram</option>
+              <option value="ounce">Ounce</option>
+              <option value="teaspoon">Teaspoon</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="servings">Number of Servings: </label>
+            <input type="number" id="servings" step="any" required/>
+          </div>
+
+          <button type="submit">Convert</button>
+        </form>
+      </main>
     </div>
   );
 }
